6b6db959721d3f3a95be7c1a731623789b0b7451
deep bhimani <deep.bhimani@shunyavkash.com>
2026-09-10 12:17:18 +0530

    Update JS

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,23 @@
+// document.querySelectorAll("[data-component]").forEach(async (element) => {
+//   const component = element.dataset.component;
+
+//   const response = await fetch(`components/${component}.html`);
+//   const html = await response.text();
+
+//   element.innerHTML = html;
+// });
+
 document.querySelectorAll("[data-component]").forEach(async (element) => {
   const component = element.dataset.component;
 
   const response = await fetch(`components/${component}.html`);
+
+  if (!response.ok) {
+    console.error(`Component "${component}" not found.`);
+    return;
+  }
+
   const html = await response.text();
 
-  element.innerHTML = html;
+  element.replaceWith(document.createRange().createContextualFragment(html));
 });
